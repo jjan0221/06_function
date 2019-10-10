dad979a38e3037901ea2f5bc48a1215e14fb969d
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-10-10 16:01:06 +0900

    06_2,3

--- main.c
+++ main.c
@@ -3,18 +3,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//함수 정의 
-void print_star()
+int sumTwo(int a, int b)
 {
-	int i;
-	for (i=0;i<10;i++)
-		printf("*");
+	int output;
+	output= a + b;
+	return output;
+}
+
+int square(int n)
+{
+	return (n*n);
+}
+
+int get_max(int a, int b)
+{
+	if (a>b) return (a);
+	else return (b);
 }
 int main(int argc, char *argv[]) {
 	
-// 함수 호출	
-	print_star();
-	print_star();
-	print_star();
+	int w, x, y, z;
+	int output;
+	int output1;
+	
+	x=2;
+	y=5;
+	z=9;
+	
+	output = sumTwo(x,y);
+	printf("sumTwo : %i\n", output);
+	
+	output1 = square(z);
+	printf("square : %i\n", output1);
+	
+	printf("get_max : %i\n", get_max(x, y) );
+	
 	return 0;
 }
