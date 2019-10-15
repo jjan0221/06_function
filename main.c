5876eb4edac6dceff4a6c07a9e5bc8daa6ae49a7
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-10-15 16:08:00 +0900

    06-7

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int n);
 
-
-int main(int argc, char *argv[]) {
-	int result;
-	result = square(5);
-	printf("%d", result);
-}
-int square(int n)
+//int combination()
+int calcCombination(int n, int r)
 {
-	return (n*n);
+	int high, low;
+	high = factorial(n);
+	low = factorial(n-r)*factorial(r);
+	
+	return (high/low);
+}
+
+//int factorial()
+int factorial(int n)
+{
+	int res = 1;
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		res = res*i;
+	}
+	return res;
+}
+
+int main(int argc, char *argv[]) 
+{
+	int n, r;
+	int res;
+	{
+		printf("input n and r : ");
+		scanf("%d %d", &n, &r);
+	}
+	res = calcCombination(n,r);
+	printf("combination result is %d\n", res);
+	
+
+	return 0;
 }
